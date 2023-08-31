e9600089a6262d8b47ebe53b8f5e477656618cea
Eric Floodh <ericfloodh@gmail.com>
2023-08-31 14:34:47 +0200

    Initial commit using mongame template.

---
+++ Main.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace TDDD23_MonogameProject;
+
+class MainClass
+{
+    static int Main(string[] args)
+    {
+        Console.WriteLine("Start");
+        using Game1 game = new Game1();
+        game.Run();
+        Console.WriteLine("End");
+        return 0;
+    }
+
+}
+
+
