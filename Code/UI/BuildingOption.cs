2ee223922b9b7acb04825d773a328fbc3814cd52
Eric Floodh <ericfloodh@gmail.com>
2023-10-10 01:30:15 +0200

    gImplemented functions to determine grid position from screen position.

--- Code/UI/BuildingOption.cs
+++ Code/UI/BuildingOption.cs
@@ -11,62 +11,109 @@ class BuildingOption
     private readonly Color bgColor = Color.Green;
 
     private Point center;
-    private Vector2 rotationCenter;
-    private float rotationAngle;
+    private float bgAngle;
+    private float fgAngle;
     private Size bgSize;
     private Size fgSize;
     Texture2D bgTexture;
     Texture2D fgTexture;
 
+    public Texture2D PlacementTexture
+    {
+        get
+        {
+            return this.fgTexture;
+        }
+    }
+
     const float floatPI = (float)Math.PI;
+
+    private readonly BuildingSelector root;
 
     public Point Center { get => center; set => center = value; }
 
+    private State state = new();
 
-    public BuildingOption(string foregroundTexturePath)
+    public float Angle{
+        get{
+            return this.bgAngle;
+        }
+    }
+
+    private class State
     {
-        this.bgSize = new Size(84, 84);
-        this.fgSize = new Size(64, 64);
+        private bool highlight = false;
 
-        this.bgTexture = Texture2D.FromFile(GameWindow.graphicsDevice, "Data/Texture/pentagon-128.png");
+        public bool Highlight { get => highlight; set => highlight = value; }
+
+        public Color Colour{
+            get{
+                if(!this.highlight)
+                    return new Color(Color.White, 0.7f);
+                else
+                    return new Color(Color.White, 1f);
+            }
+        }
+    }
+
+
+    public BuildingOption(BuildingSelector root, string foregroundTexturePath, float angle)
+    {
+        this.root = root;
+        this.bgAngle = angle;
+        this.bgSize = new Size(160, 160);
+        this.fgSize = new Size(128, 128);
+
+        // float angleInRadians = MathHelper.ToRadians(angle);
+        this.fgAngle = this.bgAngle - floatPI/2;
+        var vec = new Vector2((float)Math.Cos(this.fgAngle), (float)Math.Sin(this.fgAngle))*185;
+        this.center = root.Center + vec.ToPoint();
+
+        this.bgTexture = Texture2D.FromFile(GameWindow.graphicsDevice, "Data/Texture/pentagon3_128.png");
         this.fgTexture = Texture2D.FromFile(GameWindow.graphicsDevice, foregroundTexturePath);
     }
 
     public void UpdateByMouse(MouseState mouseState)
     {
-        if(this.Bounds.Contains(mouseState.X, mouseState.Y))
+        if(GetDistance(this.center.X, this.center.Y, mouseState.X, mouseState.Y) < this.Radius )
         {
-            // Console.WriteLine($"Hover-{this.Center.ToString()}");
+            this.root.SelectedItem = this;
+            state.Highlight = true;
             if (mouseState.LeftButton == ButtonState.Pressed)
             {
-                Console.WriteLine($"Hit-{this.Center.ToString()}");
+                Console.WriteLine($"Distance:{GetDistance(this.center.X, this.center.Y, mouseState.X, mouseState.Y)}, R:{this.Radius}");
+                Console.WriteLine(state.Colour.ToString()) ;
             }
-        }     
+        }
+        else
+        {     
+            state.Highlight = false;
+        }
     }
 
     public void Draw(SpriteBatch spriteBatch)
     {
-        var rect =  new Rectangle(this.center.X, this.center.Y, 128, 128);
-        GameWindow.spriteBatch.Draw(this.bgTexture, rect, null, Color.White, this.RotationAngle, new Vector2(this.bgTexture.Width / 2, this.bgTexture.Height / 2 + 155), SpriteEffects.None, 0f);
+        var bgDrawArea =  new Rectangle(this.root.Center.X, this.root.Center.Y, this.bgSize.Width, this.bgSize.Height);
+        GameWindow.spriteBatch.Draw(
+            this.bgTexture, bgDrawArea, null, state.Colour, this.bgAngle, 
+            new Vector2(this.bgTexture.Width / 2, this.bgTexture.Height / 2 + 155), SpriteEffects.None, 0f);
 
-        // float angleInRadians = MathHelper.ToRadians(angle);
-        float angle = this.RotationAngle - floatPI/2;
-        var vec = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle))*155;
-        var fgTopLeftPoint = new Point(this.Center.X - this.fgSize.Width/2, this.Center.Y - this.fgSize.Height/2);
-        var topLeftPoint = new Point(fgTopLeftPoint.X + (int)vec.X, fgTopLeftPoint.Y + (int)vec.Y);
-        var fgDrawArea = new Rectangle(topLeftPoint.X, topLeftPoint.Y, this.fgSize.Width, this.fgSize.Height);
-        GameWindow.spriteBatch.Draw(this.fgTexture,fgDrawArea, Sunlight.Mask);
+        var fgDrawArea =  new Rectangle(this.Center.X, this.Center.Y, this.fgSize.Width, this.fgSize.Height);
+        GameWindow.spriteBatch.Draw(
+            this.fgTexture, fgDrawArea, null, state.Colour, 0f, 
+            new Vector2(this.fgTexture.Width / 2, this.fgTexture.Height / 2), SpriteEffects.None, 0f);
     }
     
-    public Rectangle Bounds
+    private static double GetDistance(double x1, double y1, double x2, double y2)
+    {
+    return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+    }
+
+    public int Radius
     {
         get
         {
-            var bgTopLeftPoint = new Point(this.Center.X - this.bgSize.Width/2, this.Center.Y - this.bgSize.Height/2);
-            return new Rectangle(bgTopLeftPoint.X, bgTopLeftPoint.Y, bgSize.Width, bgSize.Height);
+            return 70;
         }
     }
-
-    public Vector2 RotationCenter { get => rotationCenter; set => rotationCenter = value; }
-    public float RotationAngle { get => rotationAngle; set => rotationAngle = value; }
 }
--- Code/UI/BuildingSelector.cs
+++ Code/UI/BuildingSelector.cs
@@ -17,58 +17,67 @@ class BuildingSelector
     //     throw new NotImplementedException();
     // }
 
-    private readonly Color bgColor = Color.Green;
+
     Texture2D centerTexture;
+    Texture2D centerTexturePointing;
 
     private Point center;
+
+    private Rectangle BuildingToPlaceRect;
+    private Texture2D BuildingToPlaceTexture;
     private Size displaySize;
 
-    private Rectangle drawArea;
+    private int? selectedIndex = null;
+    private BuildingOption selectedItem = null;
 
     private const string Path_Option1Texture = "Data/Texture/GreenGem.png";
     public int State = 0;
     List<BuildingOption> spriteList = new List<BuildingOption>();
-    const float floatPI = (float)Math.PI;
+
+    public Point Center
+    {
+        get
+        {
+            return this.center;
+        }
+    }
+
+    public int? SelectedIndex { get => selectedIndex; set => selectedIndex = value; }
+    internal BuildingOption SelectedItem { get => selectedItem; set => selectedItem = value; }
 
     public BuildingSelector(Size displaySize)
     {
         this.displaySize = displaySize;
         this.center = new Point(this.displaySize.Width/2, this.displaySize.Height/2);
-        this.centerTexture = Texture2D.FromFile(GameWindow.graphicsDevice, "Data/Texture/circle-128.png");
+        this.centerTexture = Texture2D.FromFile(GameWindow.graphicsDevice, "Data/Texture/center3-null.png");
+        this.centerTexturePointing = Texture2D.FromFile(GameWindow.graphicsDevice, "Data/Texture/center3.png");
 
-        this.spriteList.Add(new BuildingOption("Data/Texture/GreenGem.png"));
-        this.spriteList.Last().Center = this.center;
-        this.spriteList.Last().RotationAngle = -1.7f;
-
-        this.spriteList.Add(new BuildingOption("Data/Texture/BlueGem.png"));
-        this.spriteList.Last().Center = this.center;
-        this.spriteList.Last().RotationAngle = -0.85f;
-
-        this.spriteList.Add(new BuildingOption("Data/Texture/PurpleGem.png"));
-        this.spriteList.Last().Center = this.center;
-        this.spriteList.Last().RotationAngle = 0f;
-
-        this.spriteList.Add(new BuildingOption("Data/Texture/GreenGem.png"));
-        this.spriteList.Last().Center = this.center;
-        this.spriteList.Last().RotationAngle = 0.85f;
-
-        this.spriteList.Add(new BuildingOption("Data/Texture/BlueGem.png"));
-        this.spriteList.Last().Center = this.center;
-        this.spriteList.Last().RotationAngle = 1.7f;
-
-
+        this.spriteList.Add(new BuildingOption(this, "Data/Texture/GreenGem.png", -1.7f));
+        this.spriteList.Add(new BuildingOption(this, "Data/Texture/BlueGem.png", -0.85f));
+        this.spriteList.Add(new BuildingOption(this, "Data/Texture/PurpleGem.png", 0f));
+        this.spriteList.Add(new BuildingOption(this, "Data/Texture/GreenGem.png", 0.85f));
+        this.spriteList.Add(new BuildingOption(this, "Data/Texture/BlueGem.png", 1.7f));
     }
 
     public void UpdateByKeyboard(KeyboardState keyboardState)
     {
-        if (keyboardState.IsKeyDown(Keys.LeftShift) || keyboardState.IsKeyDown(Keys.RightShift))
+        if(this.State < 2)
         {
-             this.State = 1;
-        } 
-        else{
-            this.State = 0;
+            if (keyboardState.IsKeyDown(Keys.LeftShift) || keyboardState.IsKeyDown(Keys.RightShift))
+            {
+                this.State = 1;
+            } 
+            else{
+                this.State = 0;
+            }
+        }
+        else
+        {
+            // Add escape handling
         }
     }
+
+    private static bool selectingOption = false;
 
     public void UpdateByMouse(MouseState mouseState)
     {
@@ -78,28 +87,92 @@ class BuildingSelector
         foreach(var sprite in this.spriteList)
         {
             sprite.UpdateByMouse(mouseState);
-        }  
-    }
+        }
 
-    public void Update()
-    {
+        if(this.State == 1)
+        {
+            if(mouseState.LeftButton == ButtonState.Pressed)
+            {
+                selectingOption = true;
+            }
 
-   
+            if(selectingOption == true && mouseState.LeftButton == ButtonState.Released)
+            {
+                if(this.selectedItem != null)
+                {
+                    this.BuildingToPlaceTexture = this.selectedItem.PlacementTexture;
+                    this.State = 2;
+                }
+                else
+                {
+                    this.State = 0;
+                }
+                selectingOption = false;
+            }
+        } 
+        else if(this.State == 2)
+        {
+            this.BuildingToPlaceRect = new Rectangle(
+                        mouseState.X, mouseState.Y, this.BuildingToPlaceTexture.Width, this.BuildingToPlaceTexture.Height);
+
+            if(mouseState.LeftButton == ButtonState.Pressed)
+            {
+                selectingOption = true;
+            }
+
+            if(selectingOption == true &&  mouseState.LeftButton == ButtonState.Released)
+            {
+                // TODO Send build data
+                this.State = 0;
+            }
+
+            if(mouseState.RightButton == ButtonState.Pressed)
+            {
+                this.State = 0;
+            }
+        }
+        // Console.WriteLine($"State:{this.State}, LB:{mouseState.LeftButton.ToString()}");
     }
 
     public void Draw(SpriteBatch spriteBatch)
     {
-        if (this.State > 0)
+        if(this.State <= 0)
+            return;
+
+        if( this.State == 1)
         {
+            // Draw the options
             foreach (var sprite in this.spriteList)
             {
                 sprite.Draw(spriteBatch);
             }
 
-            int size = 128;
-            var topLeftPoint = new Point(this.center.X - size/2, this.center.Y - size/2);
-            var drawArea = new Rectangle(topLeftPoint.X, topLeftPoint.Y, size, size);
-            GameWindow.spriteBatch.Draw(this.centerTexture,drawArea, Sunlight.Mask);
+            // Draw center "knob"
+            int size = 200;
+            var drawArea =  new Rectangle(this.Center.X, this.Center.Y, size, size);
+            if(this.selectedItem == null)         
+            {
+                GameWindow.spriteBatch.Draw(
+                    this.centerTexture, drawArea, null, new Color(Color.White, 0.9f), 0f, 
+                    new Vector2(this.centerTexture.Width / 2, this.centerTexture.Height / 2), SpriteEffects.None, 0f);
+            }
+            else
+            {
+                GameWindow.spriteBatch.Draw(
+                    this.centerTexturePointing, drawArea, null, new Color(Color.White, 0.9f), this.selectedItem.Angle, 
+                    new Vector2(this.centerTexturePointing.Width / 2, this.centerTexturePointing.Height / 2), SpriteEffects.None, 0f);
+            }
+            this.selectedItem = null; // Otherwise the last selected item will be pointed at forever
         }
+        else if(this.State == 2)
+        {
+            // TODO adjust to map view
+            GameWindow.spriteBatch.Draw(
+                    this.BuildingToPlaceTexture, this.BuildingToPlaceRect, null, new Color(Color.White, 0.3f), 0f, 
+                    new Vector2(this.BuildingToPlaceTexture.Width / 2, this.BuildingToPlaceTexture.Height / 2), SpriteEffects.None, 0f);
+        }
+
+        
     }
+
 }
