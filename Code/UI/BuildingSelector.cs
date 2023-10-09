2c7c2dae6af33c830841f2c8c55bdce828249aa1
Eric Floodh <ericfloodh@gmail.com>
2023-10-10 01:30:15 +0200

    gImplemented functions to determine grid position from screen position.

--- Code/UI/BuildingSelector.cs
+++ Code/UI/BuildingSelector.cs
@@ -27,7 +27,7 @@ class BuildingSelector
 
     private const string Path_Option1Texture = "Data/Texture/GreenGem.png";
     public int State = 0;
-    List<BuildingOptionSprite> spriteList = new List<BuildingOptionSprite>();
+    List<BuildingOption> spriteList = new List<BuildingOption>();
     const float floatPI = (float)Math.PI;
 
     public BuildingSelector(Size displaySize)
@@ -36,23 +36,23 @@ class BuildingSelector
         this.center = new Point(this.displaySize.Width/2, this.displaySize.Height/2);
         this.centerTexture = Texture2D.FromFile(GameWindow.graphicsDevice, "Data/Texture/circle-128.png");
 
-        this.spriteList.Add(new BuildingOptionSprite("Data/Texture/GreenGem.png"));
+        this.spriteList.Add(new BuildingOption("Data/Texture/GreenGem.png"));
         this.spriteList.Last().Center = this.center;
         this.spriteList.Last().RotationAngle = -1.7f;
 
-        this.spriteList.Add(new BuildingOptionSprite("Data/Texture/BlueGem.png"));
+        this.spriteList.Add(new BuildingOption("Data/Texture/BlueGem.png"));
         this.spriteList.Last().Center = this.center;
         this.spriteList.Last().RotationAngle = -0.85f;
 
-        this.spriteList.Add(new BuildingOptionSprite("Data/Texture/PurpleGem.png"));
+        this.spriteList.Add(new BuildingOption("Data/Texture/PurpleGem.png"));
         this.spriteList.Last().Center = this.center;
         this.spriteList.Last().RotationAngle = 0f;
 
-        this.spriteList.Add(new BuildingOptionSprite("Data/Texture/GreenGem.png"));
+        this.spriteList.Add(new BuildingOption("Data/Texture/GreenGem.png"));
         this.spriteList.Last().Center = this.center;
         this.spriteList.Last().RotationAngle = 0.85f;
 
-        this.spriteList.Add(new BuildingOptionSprite("Data/Texture/BlueGem.png"));
+        this.spriteList.Add(new BuildingOption("Data/Texture/BlueGem.png"));
         this.spriteList.Last().Center = this.center;
         this.spriteList.Last().RotationAngle = 1.7f;
 
@@ -91,31 +91,6 @@ class BuildingSelector
     {
         if( this.State > 0)
         {
-            int segments = 5;
-            var center = new Vector2(this.center.X, this.center.Y);
-            float radius = 200f;
-            int sides = 320;
-            float startingAngle = floatPI;
-            float endAngle = 2*floatPI;
-            float radians = floatPI / segments;
-            float thickness = 3.0f;
-
-            Color color = this.bgColor;
-
-            for(float angle = startingAngle; angle <= endAngle; angle+=radians)
-            {
-                DrawArc(spriteBatch, center, radius, sides, angle, radians, color, thickness);
-                DrawArc(spriteBatch, center, 110f, sides, angle, radians, color, thickness);
-                // Console.WriteLine($"Angle : {angle}, Color : {color}");
-
-                if(color == Color.Green)
-                    color = Color.Red;
-                else
-                    color = Color.Green;
-            }
-
-            ShapeUtils.DrawCircle(spriteBatch, center, 90f, sides, Color.Blue, thickness);
-
             foreach (var sprite in this.spriteList)
             {
                 sprite.Draw(spriteBatch);
@@ -127,56 +102,4 @@ class BuildingSelector
             GameWindow.spriteBatch.Draw(this.centerTexture,drawArea, Sunlight.Mask);
         }
     }
-
-    public static void DrawArc(SpriteBatch spriteBatch, Vector2 center, float radius, int sides, float startingAngle, float radians, Color color, float thickness)
-    {
-        List<Vector2> arc = ShapeUtils.CreateArc(radius, sides, startingAngle, radians);
-        ShapeUtils.DrawPoints(spriteBatch, center, arc, color, thickness);
-    }
-
-    public void Update()
-    {
-
-   
-    }
-   public void Draw(SpriteBatch spriteBatch)
-    {
-
-
-        if( this.State > 0)
-        {
-            int segments = 5;
-            var center = new Vector2(this.center.X, this.center.Y);
-            float radius = 200f;
-            int sides = 320;
-            float startingAngle = floatPI;
-            float endAngle = 2*floatPI;
-            float radians = floatPI / segments;
-            float thickness = 3.0f;
-    
-            Color color = this.bgColor;
-
-
-            for(float angle = startingAngle; angle <= endAngle; angle+=radians)
-            {
-                DrawArc(spriteBatch, center, radius, sides, angle, radians, color, thickness);
-                DrawArc(spriteBatch, center, 110f, sides, angle, radians, color, thickness);
-                // Console.WriteLine($"Angle : {angle}, Color : {color}");
-
-                if(color == Color.Green)
-                    color = Color.Red;
-                else
-                    color = Color.Green;
-            }
-
-            ShapeUtils.DrawCircle(spriteBatch, center, 90f, sides, Color.Blue, thickness);
-        }
-    }
-
-     public static void DrawArc(SpriteBatch spriteBatch, Vector2 center, float radius, int sides, float startingAngle, float radians, Color color, float thickness)
-    {
-        List<Vector2> arc = ShapeUtils.CreateArc(radius, sides, startingAngle, radians);
-        ShapeUtils.DrawPoints(spriteBatch, center, arc, color, thickness);
-    }
-
 }
